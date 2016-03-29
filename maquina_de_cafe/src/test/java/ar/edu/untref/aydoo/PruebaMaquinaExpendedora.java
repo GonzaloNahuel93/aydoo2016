047885b24dc893c84093b5f94ceb94baa4d5670b
Gonzalo Nahuel <Gonzalo Nahuel>
2016-03-28 23:46:15 -0300

    Segunda prueba agregada en verde

--- maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/PruebaMaquinaExpendedora.java
+++ maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/PruebaMaquinaExpendedora.java
@@ -21,4 +21,20 @@ public class PruebaMaquinaExpendedora {
 
 	}
 
+	@Test
+	public void hacerTeConLecheCon3CucharadasDeAzucar(){
+
+		MaquinaExpendedora maquina = new MaquinaExpendedora();
+
+		Vaso vaso = new Vaso();
+		Assert.assertEquals("El Vaso esta vacío", 0, vaso.verContenido().size());
+
+		vaso = maquina.hacerTeConLecheConNDeAzucar(vaso, 3);
+		Assert.assertEquals("El vaso tiene 3 contenidos", 3, vaso.verContenido().size());
+		Assert.assertTrue("Hay Te", vaso.verContenido().get(0).equals("Te"));
+		Assert.assertTrue("Hay Leche", vaso.verContenido().get(1).equals("Leche"));
+		Assert.assertTrue("Hay 3 cucharadas de Azucar", vaso.verContenido().get(2).equals("3 cucharadas de Azucar"));
+
+	}
+
 }
