aa7c63f8d465a2d13ef96957b1e52a5662355591
Gonzalo Nahuel <Gonzalo Nahuel>
2016-03-28 23:53:35 -0300

    Sexta y ultima prueba agregada en verde

--- maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/PruebaMaquinaExpendedora.java
+++ maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/PruebaMaquinaExpendedora.java
@@ -80,4 +80,18 @@ public class PruebaMaquinaExpendedora {
 
 	}
 
+	@Test
+	public void hacerCafeConLecheConUnaCantidadNegativaDeAzucar(){
+
+		MaquinaExpendedora maquina = new MaquinaExpendedora();
+
+		Vaso vaso = new Vaso();
+
+		vaso = maquina.hacerCafeConLecheConNDeAzucar(vaso, -1);
+		Assert.assertEquals("El vaso tiene 2 contenidos", 2, vaso.verContenido().size());
+		Assert.assertTrue("Hay Cafe", vaso.verContenido().get(0).equals("Cafe"));
+		Assert.assertTrue("Hay Leche", vaso.verContenido().get(1).equals("Leche"));
+
+	}
+
 }
