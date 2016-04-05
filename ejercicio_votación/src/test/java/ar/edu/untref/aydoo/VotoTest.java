9739376ac48c11f49332d549c60ef773fc6b3d55
Gonzalo Nahuel <Gonzalo Nahuel>
2016-04-04 22:48:04 -0300

    VotoTest: Se crea un voto y se emite colocando provincia y candidato para despues verificar si esos datos se guardaron correctamente

---
+++ ejercicio_votación/src/test/java/ar/edu/untref/aydoo/VotoTest.java
@@ -0,0 +1,21 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VotoTest {
+
+	@Test
+	public void seCreaYSeEmiteUnVotoColocandoProvinciaDeOrigenYCandidatoElegidoComprobandoSiLosDatosIngresadosSeGuardaronDeFormaCorrecta(){
+
+		Voto voto = new Voto();
+		Candidato macri = new Candidato("Mauricio Macri");
+
+		voto.emitir(" Buenos Aires ", macri);
+
+		Assert.assertTrue(voto.getProvinciaDeOrigen().equals("BUENOS AIRES"));
+		Assert.assertTrue(voto.getCandidatoVotado().getNombreYApellido().equals("MAURICIO MACRI"));
+
+	}
+
+}
