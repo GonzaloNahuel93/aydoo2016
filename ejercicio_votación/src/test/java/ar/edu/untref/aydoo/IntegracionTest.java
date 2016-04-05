12956f1ad55b675687e7712d0342d86f8a6d101e
Gonzalo Nahuel <Gonzalo Nahuel>
2016-04-04 23:07:21 -0300

    IntegracionTest: Se vota en dos provincias pero se pide el partido ganador en una sola para saber si es correcto

--- ejercicio_votación/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
+++ ejercicio_votación/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
@@ -41,4 +41,40 @@ public class IntegracionTest {
 
 	}
 
+	@Test
+	public void seVotaEnDosProvinciasPeroSePideElPartidoGanadorEnUnaSolaParaSaberSiEsCorrecto(){
+
+		Candidato macri = new Candidato("Mauricio Macri");
+		Partido cambiemos = new Partido("Cambiemos");
+		macri.setPartido(cambiemos);
+		Candidato scioli = new Candidato("Daniel Scioli");
+		Partido fpv = new Partido("Frente para la Victoria");
+		scioli.setPartido(fpv);
+
+		Voto voto1 = new Voto();
+		voto1.emitir("Buenos Aires", macri);
+		Voto voto2 = new Voto();
+		voto2.emitir("Buenos Aires", macri);
+		Voto voto3 = new Voto();
+		voto3.emitir("Buenos Aires", macri);
+		Voto voto4 = new Voto();
+		voto4.emitir("Buenos Aires", macri);
+		Voto voto5 = new Voto();
+		voto5.emitir("Buenos Aires", macri);
+		Voto voto6 = new Voto();
+		voto6.emitir("Rio Negro", macri);
+		Voto voto7 = new Voto();
+		voto7.emitir("Rio Negro", scioli);
+		Voto voto8 = new Voto();
+		voto8.emitir("Rio Negro", scioli);
+		Voto voto9 = new Voto();
+		voto9.emitir("Rio Negro", scioli);
+		Voto voto10 = new Voto();
+		voto10.emitir("Rio Negro", scioli);
+
+		Partido partidoGanadorEnRioNegro = CentroDeComputos.getInstance().partidoGanadorEnLaProvincia("Rio Negro");	
+		Assert.assertTrue(partidoGanadorEnRioNegro.getNombre().equals("Frente para la Victoria"));
+
+	}
+
 }
