55d7f8329b31a4f5a5de5e4e42cb386b3fea6e90
Gonzalo Nahuel <Gonzalo Nahuel>
2016-04-11 04:13:44 -0300

    ClienteTest: Un cliente hace una compra y se verifica que esa compra se haya guardado correctamente

--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
@@ -15,4 +15,19 @@ public class ClienteTest {
 
 	}
 
+	@Test
+	public void unClienteHaceUnaCompraYSeVerificaQueEsaCompraSeHayaGuardadoCorrectamente(){
+
+		Cliente melisa = new Cliente("Melisa", "Bartolome Mitre 789");
+		Mes marzo = new Mes("Marzo");
+		Compra compra = new Compra(marzo);
+		Producto cocina = new Libro("Cocina para principiantes", 100);
+
+		compra.agregarProducto(cocina);
+		melisa.agregarCompra(compra);
+
+		Assert.assertEquals(1, melisa.getCompras().size());
+
+	}
+
 }
